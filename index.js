7f15bfea96a2ec9874ff262dab24c48978651b15
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-04 15:56:47 -0400

    pushing

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res) => res.send("Welcome to my Api"))
+
+const dogs = [{
+    name: "Paco",
+    breed: "chihuahua",
+    age: 34,
+},
+{
+
+    name: "Bubba",
+    breed: "labrador",
+    age: 56,
+},
+
+{    name: "Lucia",
+    breed: "yorkie",
+    age: 23,
+},
+
+{
+    name: "Bob",
+    breed: "pitbull",
+    age: 69,
+}]
+
+app.get("/", (req, res) => res.json(dogs))
 
 app.listen(4000, () => console.log("Api running on port 4000"))
